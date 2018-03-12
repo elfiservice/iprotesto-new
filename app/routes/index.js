dabec769b210a81c55cfe50f47f30b5f5e57209e
Armando Jr <elfiservice@hotmail.com>
2018-03-12 16:35:47 -0300

    feat: add cadastrar route

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,4 +2,8 @@ module.exports = function(application){
 	application.get('/', function(req, res){
 		application.app.controllers.indexCtrl.index(application, req, res);
 	});
+
+	application.post('/cadastrar', function(req, res){
+		application.app.controllers.indexCtrl.cadastrar(application, req, res);
+	});
 }
